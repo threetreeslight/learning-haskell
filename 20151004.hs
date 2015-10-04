737b6ec834044de9885540844c48bbfb266b75e5
threetreeslight <ae06710@gmail.com>
2015-10-04 16:47:51 +0900

    2015-10-04 study

--- 20151004.hs
+++ 20151004.hs
@@ -1,12 +1,148 @@
 -- Haskell Revision
 import Data.List
+import Data.Char
 
+-- whereのありがたみをちゃんと理解できていない
 -- Pattern matchの基礎であるMaybe型をしっかりやり直す
 -- folrの考え方をやり直す
+
+-- https://skami.iocikun.jp/computer/haskell/web_lecture/for_programmer/repeat_def.html
+
+-- ----------------------------------------
+-- [NEW] ++, concat, reverse
+-- ----------------------------------------
+
+-- 型で演算子化して定義している
+(.++), (.++.), (.++..) :: [a] -> [a] -> [a]
+
+-- ２項演算子としての定義
+(x : xs) .++ ys = x : (xs .++ ys)
+[] .++ ys = ys
+
+-- 初期値をysとして処理、引数xsを当たえる
+-- xs .++. ys = foldr (:) ys xs
+-- (.++.) xs ys = foldr (:) ys xs
+-- (.++.) xs ys = flip (foldr (:)) xs ys
+-- (.++.) = flip (foldr (:))
+(.++.) = flip $ foldr (:)
+
+(.++..) = curry . unfoldr $ \xys -> case xys of
+        (x : xs, ys) -> Just (x, (xs, ys))
+        (_, y : ys) -> Just (y, ([], ys))
+        _ -> Nothing
+
+concatRaw, concatF :: [[a]] -> [a]
+concatRaw (xs : xss) = xs ++ concatRaw xss
+concatRaw _ = []
+
+concatF = foldr (++) []
+
+-- whereがちゃんと理解できていない
+-- rsの頭にxを詰めていく
+reverseRaw, reverseF :: [a] -> [a]
+reverseRaw = rv []
+        where
+        rv rs (x : xs) = rv (x : rs) xs
+        rv rs _ = rs
+
+-- flip (:) (flip (:) [3,4] 2) 1
+-- flip (:) xs みたいにして xを頭にどんどん足せるようにする
+reverseF = foldl (flip (:)) []
+
+-- ----------------------------------------
+-- zip, zipWith, unzip
+-- ----------------------------------------
+
+zipRaw, zipU :: [a] -> [b] -> [(a,b)]
+zipRaw (x : xs) (y : ys) = (x, y) : zipRaw xs ys
+zipRaw _ _ = []
+
+zipU = curry . unfoldr $ \l -> case l of
+        (x : xs, y : ys) -> Just ((x,y), (xs, ys))
+        _ -> Nothing
+
+unzipRaw, unzipF :: [(a,b)] -> ([a], [b])
+unzipRaw ((x, y) : xys) = (x : xs, y : ys)
+        where (xs, ys) = unzipRaw xys
+unzipRaw _ = ([], [])
+
+unzipF = foldr (\(x, y) (xs, ys) -> (x : xs, y : ys)) ([], [])
+-- なんでwhere句分が折り畳まれるんだろう変数としては一つだから？？
+
+
+-- ----------------------------------------
+-- filter define by unfoldr and dropWhile
+-- ----------------------------------------
+
+filterU :: (a -> Bool) -> [a] -> [a]
+filterU p = unfoldr $ \l -> case dropWhile (not . p) l of
+        x : xs -> Just (x, xs)
+        _ -> Nothing
+
+-- ----------------------------------------
+-- takeWhile, dropwhile, span
+-- ----------------------------------------
+
+takeWhileRaw, takeWhileF :: (a -> Bool) -> [a] -> [a]
+takeWhileRaw p ( x : xs ) | p x = x : takeWhileRaw p xs
+takeWhileRaw _ _ = []
+
+takeWhileF p = foldr (\x -> if p x then (x :) else const [] ) []
+
+dropWhileRaw :: (a -> Bool) -> [a] -> [a]
+dropWhileRaw p (x : xs) | p x = dropWhileRaw p xs
+dropWhileRaw _ xs = xs
+
+spanRaw :: (a -> Bool) -> [a] -> ([a], [a])
+spanRaw p (x : xs) | p x = (x : t, d)
+        where (t,d) = spanRaw p xs
+spanRaw _ xs = ([], xs)
+
+-- ----------------------------------------
+-- take, drop, splitAt
+-- ----------------------------------------
+
+takeRaw, takeU :: Int -> [a] -> [a]
+takeRaw n (x : xs) | n > 0 = x : takeRaw (n - 1) xs
+takeRaw _ _ = []
+
+-- curry化することで unfoldrからのreturnを Just (x, (n, xs))
+takeU = curry . unfoldr $ \nl -> case nl of
+        (n, x : xs) | n > 0 -> Just (x, (n-1, xs))
+        _ -> Nothing
+
+dropRaw  :: Int -> [a] -> [a]
+dropRaw n (x : xs) | n > 0 = dropRaw (n - 1) xs
+dropRaw _ xs = xs
+
+splitAtRaw :: Int -> [a] -> ([a], [a])
+splitAtRaw n (x : xs) | n > 0 = (x : t, d)
+          where (t,d) = splitAtRaw (n - 1) xs
+splitAtRaw _ xs = ([], xs)
+
 
 -- ----------------------------------------
 -- filter, partition
 -- ----------------------------------------
+
+filterRaw, filterF :: (a -> Bool) -> [a] -> [a]
+
+filterRaw p (x : xs)
+        | p x = x : filterRaw p xs
+        | otherwise = filterRaw p xs
+filterRaw _ _ = []
+
+filterF p = foldr (\x -> if p x then (x : ) else id) []
+
+partitionRaw, partitionF :: (a -> Bool) -> [a] -> ([a], [a])
+
+partitionRaw p (x : xs)
+        | p x = (x : ts, es)
+        | otherwise = (ts, x : es)
+        where (ts, es) = partitionRaw p xs
+partitionRaw _ _ = ([], [])
+
+partitionF p = foldr (\x (ts, es) -> if p x then (x :ts, es) else (ts, x : es)) ([], [])
 
 -- ----------------------------------------
 -- map
